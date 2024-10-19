baf43e83ece5e575e3d52c2ccbdcf38fa377f98e
Reece Kelly <reecekelly2004@outlook.com>
2024-10-19 21:39:43 +1000

    Add incomplete test function for GetAllChildFolders

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -28,3 +28,29 @@ func Test_folder_GetFoldersByOrgID(t *testing.T) {
 	}
 }
 
+func Test_folder_GetAllChildFolders(t *testing.T) {
+	t.Parallel()
+	tests := [...]struct {
+		name    string
+		orgID   uuid.UUID
+		folders []folder.Folder
+		want    []folder.Folder
+	}{
+		// TODO: Add tests here
+		{
+			name:  "clear-arclight",
+			orgID: uuid.Must(uuid.FromString("38b9879b-f73b-4b0e-b9d9-4fc4c23643a7")),
+			folders: []folder.Folder{
+			},
+			want: []folder.Folder{
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// f := folder.NewDriver(tt.folders)
+			// get := f.GetAllChildFolders(tt.orgID)
+
+		})
+	}
+}
