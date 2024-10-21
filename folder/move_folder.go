9e159bfb57fc236ac2d9ee63797a14c6d1374979
Reece Kelly <reecekelly2004@outlook.com>
2024-10-21 14:40:58 +1000

    Add code to MoveFolder method and test

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -1,7 +1,40 @@
 package folder
 
-func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
-	// Your code here...
+import (
+	"strings"
+)
 
-	return []Folder{}, nil
+func (f *driver) MoveFolder(name string, dst string) []Folder {
+	// Your code here...
+	allFolders := f.folders
+
+	modifiedFolders := []Folder{}
+
+	var destinationPath string = ""
+
+	for _, folder := range allFolders {
+		if folder.Name == dst {
+			destinationPath = folder.Paths
+			break // break the loop to stop it from unnecessarily checking rest of list
+		}
+	}
+
+	var newPath string
+
+	for _, folder := range allFolders {
+		// This section modifies the path of the target folder
+		if folder.Name == name {
+			newPath = destinationPath + "." + folder.Name // Not sure if this is the correct way to add strings together
+			folder.Paths = newPath
+		
+		// This section modifies the path of the child folders of the parent folder
+		} else if strings.Contains(folder.Paths, name) {
+			indexOfParentFolderName := strings.Index(folder.Paths, name) // Find the index of the parent folder in the child folders path
+			folder.Paths = destinationPath + "." + folder.Paths[indexOfParentFolderName:] // Create new path with destination path as the beginning
+		}
+
+		modifiedFolders = append(modifiedFolders, folder)
+	}
+
+	return modifiedFolders
 }
--- folder/move_folder_test.go
+++ folder/move_folder_test.go
@@ -2,8 +2,77 @@ package folder_test
 
 import (
 	"testing"
+
+	"github.com/georgechieng-sc/interns-2022/folder"
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_folder_MoveFolder(t *testing.T) {
 	// TODO: your tests here
+	t.Parallel()
+	tests := [...]struct {
+		name      string
+		dst     string
+		folders   []folder.Folder
+		want      []folder.Folder
+	}{
+		// TODO: Add tests here
+		{
+			name: "nearby-secret",
+			dst: "creatives-calphunter",
+			folders: []folder.Folder{
+				{
+					Name:  "creative-scalphunter",
+					OrgId: uuid.Must(uuid.FromString("38b9879b-f73b-4b0e-b9d9-4fc4c23643a7")),
+					Paths: "creative-scalphunter",
+				},
+				{
+					Name: "noble-vixen",
+					OrgId: uuid.Must(uuid.FromString("c1556e17-b7c0-45a3-a6ae-9546248fb17a")),
+					Paths: "noble-vixen",
+				},
+				{
+					Name: "nearby-secret",
+					OrgId: uuid.Must(uuid.FromString("c1556e17-b7c0-45a3-a6ae-9546248fb17a")),
+					Paths: "noble-vixen.nearby-secret",
+				},
+				{
+					Name: "magnetic-sinister-six",
+					OrgId: uuid.Must(uuid.FromString("c1556e17-b7c0-45a3-a6ae-9546248fb17a")),
+					Paths: "noble-vixen.nearby-secret.magnetic-sinister-six",
+				},
+			},
+			want: []folder.Folder{
+				{
+					Name:  "creative scalphunter",
+					OrgId: uuid.Must(uuid.FromString("38b9879b-f73b-4b0e-b9d9-4fc4c23643a7")),
+					Paths: "creative-scalphunter",
+				},
+				{
+					Name: "noble-vixen",
+					OrgId: uuid.Must(uuid.FromString("c1556e17-b7c0-45a3-a6ae-9546248fb17a")),
+					Paths: "noble-vixen",
+				},
+				{
+					Name: "nearby-secret",
+					OrgId: uuid.Must(uuid.FromString("c1556e17-b7c0-45a3-a6ae-9546248fb17a")),
+					Paths: "creative-scalphunter.nearby-secret",
+				},
+				{
+					Name: "magnetic-sinister-six",
+					OrgId: uuid.Must(uuid.FromString("c1556e17-b7c0-45a3-a6ae-9546248fb17a")),
+					Paths: "creative-scalphunter.nearby-secret.magnetic-sinister-six",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := folder.NewDriver(tt.folders)
+			get := f.MoveFolder(tt.name, tt.dst)
+
+			assert.Equal(t, tt.want, get)
+		})
+	}
 }
